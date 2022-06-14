1b6e1337f91e47b3e05f2eab4c6c3c274d70a1ed
Victor Martins <victor.martins.duarte@gmail.com>
2022-06-14 18:51:45 -0300

    Fix: Update password

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Senha.10',
+  password: process.env.MYSQL_PASSWORD || '207455',
   host: process.env.MYSQL_host || 'localhost',
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
