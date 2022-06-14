62c640ae23d083ef36ea3ea4e6584c48ab943034
Victor Martins <victor.martins.duarte@gmail.com>
2022-06-14 19:33:32 -0300

    Chore: Ongoing fix to github evaluator

--- models/connection.js
+++ models/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '207455',
-  host: process.env.MYSQL_host || 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_host,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
